eb60d3336b084d5da7d9c7d2479565573208f530
dickeyxxx <jeff@dickey.xxx>
2013-09-08 19:58:05 -0700

    added test for caching_controller

--- test/functional/caching_controller_test.rb
+++ test/functional/caching_controller_test.rb
@@ -6,4 +6,10 @@ class CachingControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "has correct values" do
+    get :index
+    values = assigns(:values)
+    assert_in_delta(0.09983341664682817, values[0], 0.000001)
+  end
+
 end
